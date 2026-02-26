6245a005237789e07b8af6a6cdf3118ade1a61c3
Mashyumaru <marchand.foot@gmail.com>
2026-02-26 16:20:22 +0100

    Exo4

--- src/main/java/org/sebsy/demo/escaperooms/bll/TreasureService.java
+++ src/main/java/org/sebsy/demo/escaperooms/bll/TreasureService.java
@@ -1,5 +1,7 @@
 package org.sebsy.demo.escaperooms.bll;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface TreasureService {
 
     public String ouvrir();
--- src/main/java/org/sebsy/demo/escaperooms/controller/TreasureRoomController.java
+++ src/main/java/org/sebsy/demo/escaperooms/controller/TreasureRoomController.java
@@ -1,10 +1,18 @@
 package org.sebsy.demo.escaperooms.controller;
 
 import org.sebsy.demo.escaperooms.bll.TreasureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Component
 public class TreasureRoomController {
     private TreasureService tresorService;
 
+    @Autowired
+    public TreasureRoomController(TreasureService tresorService) {
+        this.tresorService = tresorService;
+    }
     public String fin() {
         return tresorService.ouvrir();
     }
